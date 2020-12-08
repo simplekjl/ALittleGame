f9447c38eabc1030ccf236225f7a2e7effbc2de8
Jose Crisostomo <jl.cs606@gmail.com>
2020-12-08 15:56:25 +0100

    Added main Ui for the mini game
    Added roboto font
    Added icons
    Updated strings.xml

--- app/src/main/java/com/simplekjl/fallingwords/MainActivity.kt
+++ app/src/main/java/com/simplekjl/fallingwords/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.simplekjl.fallingwords
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        object : CountDownTimer(3000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                val number = millisUntilFinished / 1000
+                startCounter.text = number.toString()
+            }
+
+            override fun onFinish() {
+                startCounter.text = getString(R.string.start_label)
+                // TODO notify viewModel to start game and hide views
+            }
+        }.start()
     }
 }
