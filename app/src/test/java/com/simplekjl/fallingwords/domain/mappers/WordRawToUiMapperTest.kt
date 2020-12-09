242dce2e35e1fe3f751471dcc262f75b4eb95018
Jose Crisostomo <jl.cs606@gmail.com>
2020-12-09 01:26:57 +0100

    Completed Mapper testing

--- app/src/test/java/com/simplekjl/fallingwords/domain/mappers/WordRawToUiMapperTest.kt
+++ app/src/test/java/com/simplekjl/fallingwords/domain/mappers/WordRawToUiMapperTest.kt
@@ -1,25 +1,27 @@
 package com.simplekjl.fallingwords.domain.mappers
 
+import com.google.common.truth.Truth.assertThat
+import com.simplekjl.fallingwords.data.model.Word
 import com.simplekjl.fallingwords.ui.model.WordViewEntity
 import org.junit.Test
-import kotlin.random.Random
 
 internal class WordRawToUiMapperTest {
 
+    private val mapperToTest = WordRawToUiMapper()
+
     @Test
     fun mapToUi() {
-
+        val list = listOf(Word("word", "one"), Word("wordTwo", "two"))
+        val result = mapperToTest.mapToUi(list[1])
+        assertThat(result).isInstanceOf(WordViewEntity::class.java)
     }
 
     @Test
     fun mapListRawToUi() {
+        val list = listOf(Word("word", "one"), Word("wordTwo", "two"))
+        val result = mapperToTest.mapListRawToUi(list)
+        assertThat(result[0]).isInstanceOf(WordViewEntity::class.java)
     }
 
-    private fun createListOfRandomWords(): List<WordViewEntity> {
-        val list = mutableListOf<WordViewEntity>()
-        for (i in 0..16) {
-            list.add(WordViewEntity(Random.nextInt().toString(), ""))
-        }
-        return list
-    }
+
 }
